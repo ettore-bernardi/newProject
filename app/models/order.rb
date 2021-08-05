d6064f68623be346dde731a8bdb4b4628e3e012f
Ettore <ettore.contas@gmail.com>
2021-08-05 19:18:08 -0300

    cocoon working

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,6 @@
 class Order < ApplicationRecord
   belongs_to :user
 
-  has_many :items, inverse_of: :order
+  has_many :items, inverse_of: :order, dependent: :destroy
     accepts_nested_attributes_for :items, reject_if: :all_blank, allow_destroy: true
 end
