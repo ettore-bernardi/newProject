87fd83a5cae6cbf82db7603e662d675f8cd2c678
Ettore <ettore.contas@gmail.com>
2021-08-31 18:46:36 -0300

    finished i18n implementation

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ module Cachorro
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-    config.i18n.default_locale = 'pt-BR'
+    config.i18n.default_locale = 'en'
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
